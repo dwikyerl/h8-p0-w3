5dde241e8c91c52f4d8f53e3c4639df36db40c5a
Dwiky Erlangga <dwikyerl@gmail.com>
2018-03-25 15:49:58 +0700

    Finished exercise 7 : Hitung Jumlah Kata

---
+++ exercise-7.js
@@ -0,0 +1,18 @@
+/*
+  Logic Challenge - Menghitung Jumlah Kata
+*/
+
+/**
+ * @param {string} kalimat - any sentence
+ */
+
+function hitungJumlahKata(kalimat) {
+  return kalimat.split(" ").length;
+}
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
